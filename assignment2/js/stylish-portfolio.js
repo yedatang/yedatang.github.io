1841e48ca0b039c50ee781b6d447c80e350238b4
Yedda <yedantang@163.com>
2018-10-28 21:43:46 +0800

    assignment2
    jspuzzle

---
+++ assignment2/js/stylish-portfolio.js
@@ -0,0 +1,10 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
