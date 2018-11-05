060dea4442ff59a6bddba7ca90ab7f4449d6fe3d
Yedda <yedantang@163.com>
2018-11-05 19:59:56 +0800

    change
    change

--- assignment5/viewcounts.js
+++ assignment5/viewcounts.js
@@ -40,5 +40,24 @@ $("button#roll_up").click(function() {
                 ]
             } ); // end dataTable
           }); // end .getJSON
+
+          var chart = c3.generate({
+          data: {
+          columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+        ],
+          axes: {
+          data1: 'y',
+          data2: 'y2'
+        }
+    },
+          axis: {
+          y2: {
+          show: true
+        }
+    }
+});
+
        }); // end button
     }); // document ready
