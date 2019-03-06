084c8a48dbf73290337ec3f592d00a14e35fb298
Yedda <yedantang@163.com>
2019-03-06 20:36:41 +0800

    update
    update

---
+++ com5940/bookstoremap/city_page.php
@@ -0,0 +1,41 @@
+<?php /* Template Name: city*/ ?>
+<?php get_header(); ?>
+<body>
+	<div class="container"><br><br><br><br>
+		<div class="row mt presentation centered">
+
+	<h1 class="centered" style="font-size:30px;">請選擇您所在的城市</h1>
+	<p>網站開發中，敬請期待更多</p>
+	<hr>
+
+
+		<div class="row centered">
+		<a href="bookstores.html" style="color:grey;"><button type="button" class="btn btn btn-light btn-lg">廣 州</button>&nbsp&nbsp&nbsp</a>
+		<a href="events.html" style="color:grey;"><button type="button" class="btn btn btn-light btn-lg">廈 門</button>&nbsp&nbsp&nbsp</a>
+	<a href="allreadtrial.html" style="color:grey;"><button type="button" class="btn btn btn-light btn-lg">台 灣</button>&nbsp&nbsp&nbsp</a><br><br>
+		<a href="market.html" style="color:grey;"><button type="button" class="btn btn btn-light btn-lg">上 海</button>&nbsp&nbsp&nbsp</a>
+		<a href="feature.html" style="color:grey;"><button type="button" class="btn btn btn-light btn-lg">北 京</button>&nbsp&nbsp&nbsp</a>
+		<a href="share.html" style="color:grey;"><button type="button" class="btn btn btn-light btn-lg">成 都</button>&nbsp&nbsp&nbsp</a><br><br>
+	</div>
+
+	<iframe class="map-embed" src="neweventmap.html" frameborder="0" onmousewheel="" width="100%" height="600" style="background: transparent;"></iframe>
+
+
+		<br><br>
+
+		<!-- end:Clients section -->
+<section id="end" class="section-footer">
+	<div class="container centered ptb">
+	<div class="col-md-6 col-md-offset-3">
+			<h3><strong>BOOKSTORE+</strong></h3>
+			<p>閱讀，并體驗更多<br>版权為Bookstore+所有</p>
+			<p class="mt">
+		<p>聯繫我們</p>
+		<a href="#"><i class="fa fa-dribbble"></i></a>
+		<a href="#"><i class="fa fa-instagram"></i></a>
+		<a href="#"><i class="fa fa-twitter"></i></a>
+		</div>
+		</div>
+	</section>
+</body>
+<?php get_footer(); ?>
